f6c83a7451247457679a8b1b54bf7abf767496fe
Dominic Smith <dom@domsmith.co.uk>
2020-09-21 21:13:22 +0100

    Testing output for Slack

--- IRNSchedule/IRNSchedule.py
+++ IRNSchedule/IRNSchedule.py
@@ -5,19 +5,34 @@ from croniter import croniter
 from dateutil import rrule
 
 
-file = open('irn.html', 'w')
-my_user_cron = CronTab(tabfile='crontab-copy')
+file = open('/home/pi/105irnschedule/irn.html', 'w')
+my_user_cron = CronTab(tabfile='/home/pi/105irnschedule/crontab')
 
 now = datetime.now()
 next_hour = datetime(now.year, now.month, now.day, now.hour, 0)
 
 sevenDaysLater = now + timedelta(days=7)
-file.write("<html><head><title>Cambridge 105 - IRN scheduling</title></head><body><h1>IRN TOTH Schedule</h1><p>Generated at: ")
+file.write("<html><head><title>Cambridge 105 - IRN scheduling</title>\r\n")
+file.write("<style>\r\n.irn {background-color:GoldenRod;}\r\n.none {background-color:LightCoral;}\r\n.unknown {background-color:LightGrey;}\r\n</style>")
+file.write ("</head><body><h1>IRN TOTH Schedule</h1><p>Generated at: ")
 file.write(now.strftime("%H:%M on %d %b %Y"))
-file.write("</p><table border=\"1\"><thead><tr><th>Hour</th><th>Schedule</tr></thead><tbody>")
+file.write("</p>\r\n")
+
+print("Working on rules...")
+lastday = -1
+currentday = now.strftime("%W")
+hasToday = False
+todayOutput = ""
 
 for dt in rrule.rrule(rrule.HOURLY, dtstart=next_hour, until=sevenDaysLater):
     hours = dt+timedelta(hours=1)
+    currentday = hours.strftime("%w")
+    if (currentday != lastday):
+        if (lastday != -1):
+            file.write ("</tbody></table>\r\n")
+			hasToday = True
+        file.write ("<h2>" + hours.strftime("%a %d %b %Y") + "</h2>")
+        file.write ("\r\n<table border=\"1\"><thead><tr><th>Hour</th><th>Schedule</tr></thead><tbody>")
     hasResult = False
     nextDate = sevenDaysLater
     hourCmd = ""
@@ -34,16 +49,30 @@ for dt in rrule.rrule(rrule.HOURLY, dtstart=next_hour, until=sevenDaysLater):
                   hourCmd = job.command
                   if ("NEWSONWED" in job.command):
                       hourCmd = "(Depends on NEWSONWED setting)"
+                      rowclass="unknown"
 
-    hoursStr = hours.strftime("%a %d %b %Y %H:%M")
+    hoursStr = hours.strftime("%H:%M")
     if (" on on " in hourCmd):
         hourCmd = "IRN, weather-next"
+        rowclass = "irn"
+		if (hasToday == False):
+		    todayOutput = todayOutput + hoursStr + ", "
     if (" on off " in hourCmd):
         hourCmd = "IRN only"
+        rowclass="irn" 
+		if (hasToday == False):
+		    todayOutput = todayOutput + hoursStr + ", "
     if (" on flp " in hourCmd):
         hourCmd = "IRN, weather-now"
+        rowclass="irn"
+		if (hasToday == False):
+		    todayOutput = todayOutput + hoursStr + ", "
     if (minsOffset > 59):
         hourCmd = "-"
-    file.write("<tr><td>"+ hoursStr+"</td><td>"+ hourCmd+"</td></tr>")
+        rowclass="none"
+    file.write("<tr class=\"" + rowclass + "\"><td>"+ hoursStr+"</td><td>"+ hourCmd+"</td></tr>\r\n")
+    lastday = hours.strftime("%w")
 file.write("</tbody></table>")
+print("Done")
+print(todayOutput)
 file.close()
